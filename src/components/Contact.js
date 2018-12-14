fb23485fd137300691bbc06f4c4d585ff565db99
Milad Mikeal <miladmikeal@gmail.com>
2018-12-13 16:36:28 -0800

    Enable state change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,24 +3,35 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
+        <h4>
+          {name}
+          <i
+            onClick={() => this.setState({
+              showContactInfo: !this.state.showContactInfo
+            })}
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {showContactInfo ? (<ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        </ul>) : null}
       </div>
     );
   }
 }
 
-Contact.PropTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
---
+++ src/components/Contacts.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+
+class Contacts extends Component {
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: 'John Doe',
+                email: 'jdoe@gmail.com',
+                phone: '555-555-5555'
+            },
+            {
+                id: 2,
+                name: 'Karen Williams',
+                email: 'karen@gmail.com',
+                phone: '555-444-4444'
+            },
+            {
+                id: 3,
+                name: 'Henry Johnson',
+                email: 'henry@gmail.com',
+                phone: '555-333-3333'
+            }
+        ]
+    }
+
+    render() {
+        const { contacts } = this.state;
+        return (
+            <React.Fragment>
+                {contacts.map(contact => (
+                    <Contact
+                        key={contact.id}
+                        contact={contact}
+                    />
+                ))}
+            </React.Fragment>
+        )
+    }
+}
+
+export default Contacts;
