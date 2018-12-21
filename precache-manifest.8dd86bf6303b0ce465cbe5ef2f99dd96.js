d39d0148bdc645cdafdc966f8d6701e049313cc3
Milad Mikeal <miladmikeal@gmail.com>
2018-12-20 18:19:28 -0800

    Updates

---
+++ precache-manifest.8dd86bf6303b0ce465cbe5ef2f99dd96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "78eda3167b6c108fa1a3",
+    "url": "/contactmanager/static/js/main.78eda316.chunk.js"
+  },
+  {
+    "revision": "4a96dc24152eeda72e41",
+    "url": "/contactmanager/static/js/1.4a96dc24.chunk.js"
+  },
+  {
+    "revision": "78eda3167b6c108fa1a3",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "4a96dc24152eeda72e41",
+    "url": "/contactmanager/static/css/1.575d6300.chunk.css"
+  },
+  {
+    "revision": "a58c3c3159cea56151eb2fe3d38c94c9",
+    "url": "/contactmanager/index.html"
+  }
+];
